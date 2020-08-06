91de4aa64fba70fc417354b754334379d7ad1b93
nnhaipy <69191765+nnhaipy@users.noreply.github.com>
2020-08-07 02:23:25 +0700

    Demo
    Demo Requi

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+import MyApp from './Src/App';
+
+const App: () => React$Node = () => {
+  return (
+    <View>
+      <MyApp />
+    </View>
+  );
+};
+
+export default App;
